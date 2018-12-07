f886c0ba500c15db9b9152786f0bd9bb33405bd9
Student <Student@DESKTOP-JOKA8G8>
2018-12-07 11:01:55 +0530

    dlldemo

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -7,14 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyCalculator;
+
 
 namespace WindowsFormsApp5
 {
     public partial class Form1 : Form
     {
+      
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(textBox1.Text);
+            int number2 = Convert.ToInt32(textBox2.Text);
+            Class1 myclas = new Class1();
+            textBox3.Text = Convert.ToString(myclas.Add(number1, number2));
         }
     }
 }
